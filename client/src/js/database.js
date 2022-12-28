73ab1c47d3d82f35dd317f3159ff8783e563a0a0
areed98 <austinjreed89@gmail.com>
2022-12-28 17:31:19 -0500

    database.js updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,5 @@
 import { openDB } from 'idb';
-
+// Initialize DB
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Put into database to edit rather than add
+export const putDb = async (content) => {
+  console.log('PUT into database');  
 
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content: content });
+  const result = await request;
+  console.log('Saved to DB', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Get all items from DB
+export const getDb = async () => {
+  console.log('GET from database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.vale', result);
+  return result;
+}
+
 
 initdb();
